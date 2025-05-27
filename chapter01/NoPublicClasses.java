51d437957463fa972211ef2ea285e11dac232aac
Carlos Estrada <carlos.estrada@carestra.se>
2025-05-27 16:03:45 +0200

    Source file contains Java objects

---
+++ chapter01/NoPublicClasses.java
@@ -0,0 +1,18 @@
+/*
+* Source file can define at least one class.
+* Java does not required that the class has public access modifier.
+* Compiling this class 'javac Animal.java' will produce 3 .class files.
+* A source file can only contain at most 1 class with public access modifier.
+*/ 
+class Animalito {
+String name;
+}
+
+class Pero {
+String name;
+}
+
+class Gato {
+String name;
+}
+
