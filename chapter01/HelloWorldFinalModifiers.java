430f5d5096fef3886279ec26ba3d8a1186a0101a
Carlos Estrada <carlos.estrada@carestra.se>
2025-05-27 16:05:18 +0200

    Main() - parameter options.

---
+++ chapter01/HelloWorldFinalModifiers.java
@@ -0,0 +1,6 @@
+public class HelloWorldFinalModifiers {
+   public final static void main(final String[] args) {
+	System.out.println("Hello World");
+   }
+}
+
---
+++ chapter01/HelloWorldVarargs.java
@@ -0,0 +1,6 @@
+public class HelloWorldVarargs {
+   public static void main(String... inputs) {
+	System.out.println("Hello World");
+   }
+}
+
